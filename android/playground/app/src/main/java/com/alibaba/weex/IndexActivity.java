f0a1e20e7b0cf596b9c2f66d0333ef8a0c828b57
leiming.yanlm <leiming.yanlm@alibaba-inc.com>
2017-06-23 10:24:34 +0800

    gradle升级。

--- android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
+++ android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
@@ -52,7 +52,7 @@ public class IndexActivity extends AbstractWeexActivity {
   private static final String TAG = "IndexActivity";
   private static final int CAMERA_PERMISSION_REQUEST_CODE = 0x1;
   private static final String DEFAULT_IP = "your_current_IP";
-  private static String sCurrentIp = "30.15.48.147"; // your_current_IP
+  private static String sCurrentIp = "30.15.48.15";//DEFAULT_IP // your_current_IP
 
   private ProgressBar mProgressBar;
   private TextView mTipView;
